aa1bc637c710bf2db0704cec2265f956264167c6
Artyflex <denis.seo@nxt.ru>
2022-09-15 16:58:44 +0300

    task2

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -4,3 +4,31 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+
+test_data = [
+    [[2, 3, 4, 5, 6], [12, 15, 16]],
+    [[2, 3, 5, 6], [12, 15]],    
+]
+
+
+def calc_pair_multiply(list):
+    i, j = 0, len(list) - 1
+    
+    result = []
+
+    while i <= j:
+        result.append(list[i] * list[j])
+        i = i + 1
+        j = j - 1        
+
+
+    print(result)
+
+    return result
+    
+
+
+
+for list, expected in test_data:
+    assert calc_pair_multiply(list) == expected
