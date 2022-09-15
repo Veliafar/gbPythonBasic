b8d2152b83635257caa2c89cc70c0ae4888faf2c
Artyflex <denis.seo@nxt.ru>
2022-09-15 17:31:05 +0300

    task4

--- lesson3/task4.py
+++ lesson3/task4.py
@@ -5,3 +5,25 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+
+test_data = [
+    [45, '101101'],
+    [3, '11'],
+    [2, '10']
+]
+
+
+def decadeToBinary(num: int):
+    string = ''
+
+    while num:
+        rest = num % 2
+        string = str(rest) + string
+        num = num // 2
+
+    return string
+
+
+for list, expected in test_data:
+    assert decadeToBinary(list) == expected
